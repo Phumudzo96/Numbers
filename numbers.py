d49c1870d7a24f5156701f0ea178ff032122f886
Phumudzo <103152398+Phumudzo96@users.noreply.github.com>
2023-02-14 17:14:23 +0200

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,17 @@
+#Ask the user to enter numbers three times
+num1 = int(input("Enter a number? "))
+num2 = int(input("Enter a number? "))
+num3 = int(input("Enter a number? "))
+
+#Add,subtract,divide and mulitple the numbers given by the user
+sum1 = num1 + num2 + num3
+sum2 = num1 - num2
+sum3 = num3 * num1
+num4 = num1 + num2 + num3
+sum4 = num4 / num3
+
+#Print the results 
+print(int(sum1))
+print(int(sum2))
+print(int(sum3))
+print(int(sum4))
